472e495f231ddafc78482f3b4886bae86bf46175
afiab <abidica30@stuy.edu>
2023-09-13 18:50:40 -0400

    added back/forth buttons to change cards, added functionality to the back button

--- flashcards.py
+++ flashcards.py
@@ -4,15 +4,15 @@ Parse text file for front/back of cards
 creates a display with text
 """
 from tkinter import *
-GLOBAL_CURRENT_INDEX = 0 #the current question
 
 #Functions:
 def createList(filename):
     """parse txt file contents into respective lists"""
-    global GLOBAL_QUESTIONS_LIST, GLOBAL_ANSWERS_LIST, GLOBAL_CURRENT_SIDE
+    global GLOBAL_QUESTIONS_LIST, GLOBAL_ANSWERS_LIST, GLOBAL_CURRENT_SIDE, GLOBAL_CURRENT_INDEX
     GLOBAL_QUESTIONS_LIST = [] #Format: [q1, ..., qn]
     GLOBAL_ANSWERS_LIST = [] #Format: [a1, ..., an]
     GLOBAL_CURRENT_SIDE = True #True=question, False=answer
+    GLOBAL_CURRENT_INDEX = 0
     isQuestion = True
     with open(filename) as contents: 
         for line in contents:
@@ -37,28 +37,24 @@ def showAnswerList():
     global GLOBAL_ANSWERS_LIST
     return GLOBAL_ANSWERS_LIST
 
-def showQuestion(qNum=GLOBAL_CURRENT_INDEX):
+def showQuestion(qNum):
     """accessor method for specified question numbered qNum"""
     global GLOBAL_QUESTIONS_LIST
-    if qNum>len(GLOBAL_QUESTIONS_LIST) or qNum<0:
-        return showQuestion(0)
-    return GLOBAL_QUESTIONS_LIST[int(qNum)]
+    words['text']=GLOBAL_QUESTIONS_LIST[int(qNum)]
 
-def showAnswer(aNum=GLOBAL_CURRENT_INDEX):
+def showAnswer(aNum):
     """accessor method for specified answer numbered aNum"""
-    global GLOBAL_ANSWERS_LIST
-    if aNum>len(GLOBAL_ANSWERS_LIST)or aNum<0:
-        return showAnswer(0)
-    return GLOBAL_ANSWERS_LIST[int(aNum)]
+    global GLOBAL_ANSWERS_LIST        
+    words['text']=GLOBAL_ANSWERS_LIST[int(aNum)]
 
 #tkinter functions
 def flip():
     """This function flips the card, and updates GLOBAL_CURRENT_SIDE"""
-    global GLOBAL_CURRENT_SIDE
+    global GLOBAL_CURRENT_SIDE, GLOBAL_CURRENT_INDEX
     if GLOBAL_CURRENT_SIDE:
-        words['text']=showQuestion()
+        showQuestion(GLOBAL_CURRENT_INDEX)
     else:
-        words['text']=showAnswer()
+        showAnswer(GLOBAL_CURRENT_INDEX)
     GLOBAL_CURRENT_SIDE = not GLOBAL_CURRENT_SIDE
 
 def record_file():
@@ -68,6 +64,17 @@ def record_file():
     createList(GLOBAL_FILE_NAME)
     currentCard['text'] = "Card {0}/{1}".format(GLOBAL_CURRENT_INDEX,len(GLOBAL_QUESTIONS_LIST)-1)
     flip()
+
+def go_Back():
+    """This function goes to the previous card"""
+    global GLOBAL_CURRENT_INDEX, GLOBAL_CURRENT_SIDE
+    if GLOBAL_CURRENT_INDEX==0:
+        GLOBAL_CURRENT_INDEX=len(GLOBAL_QUESTIONS_LIST)-1
+    else:
+        GLOBAL_CURRENT_INDEX-=1
+    currentCard['text'] = "Card {0}/{1}".format(GLOBAL_CURRENT_INDEX,len(GLOBAL_QUESTIONS_LIST)-1)
+    GLOBAL_CURRENT_SIDE = False
+    showQuestion(GLOBAL_CURRENT_INDEX)
 
 root = Tk()
 root.geometry("400x200")#minimum window size
@@ -99,11 +106,16 @@ switch.pack(side=TOP)
 #track is section with general stats and prev/next button
 track = Frame(root)
 currentCard = Label(track)
-currentCard['text'] = "Card {0}/{1}".format(GLOBAL_CURRENT_INDEX,0)
-currentCard.pack(side=BOTTOM, anchor=W)
-
-#maybe another section here for 
-#buttons to go to next/prev card
+currentCard['text'] = "Card {0}/{0}".format(0)
+currentCard.pack(side=LEFT, anchor=S)
+goNext = Button(track)
+goNext['text']='>'
+#goNext['command'] = #idk
+goNext.pack(side=RIGHT,anchor=S)
+goBack = Button(track)
+goBack['text']='<'
+goBack['command'] = go_Back
+goBack.pack(side=RIGHT,anchor=S)
 
 #determines where the sections are located
 getFile.pack(side=TOP, expand=YES, fill=BOTH)
